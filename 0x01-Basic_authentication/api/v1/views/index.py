927be067a65f563b857dce1ce9546f925979c06e
Kaywuyep <wuyepkate@gmail.com>
2024-06-03 11:44:36 +0000

    tasks

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,21 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
+def unauthorized():
+    """
+    GET /api/v1/unauthorized
+    Return:
+        Endpoint to raise a 401 error """
+    return abort(401)
+
+
+@app_views.route('/forbidden/', methods=['GET'], strict_slashes=False)
+def forbidden():
+    """
+    GET GET /api/v1/forbidden
+    Return:
+        Endpoint to raise a 403 error """
+    return abort(403)
